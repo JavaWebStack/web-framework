7d6e8c19e15820bb22faf73703018c1808628a13
Julian Gojani <julian@gojani.xyz>
2021-03-20 16:00:28 +0100

    Added simple WebFrameworkTest class

---
+++ src/main/java/org/javawebstack/framework/testing/WebFrameworkTest.java
@@ -0,0 +1,19 @@
+package org.javawebstack.framework.testing;
+
+import org.javawebstack.framework.WebApplication;
+import org.javawebstack.httpserver.test.HTTPTest;
+
+public abstract class WebFrameworkTest {
+
+    private HTTPTest httpTest;
+    private WebApplication webApplication;
+
+    public WebFrameworkTest(WebApplication webApplication){
+        this.webApplication = webApplication;
+        httpTest = new HTTPTest(webApplication.getServer()) {};
+    }
+
+    public HTTPTest getHttpTest() {
+        return httpTest;
+    }
+}
