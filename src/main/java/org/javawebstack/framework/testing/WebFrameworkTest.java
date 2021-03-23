77d3730443b4d8198f8668990c948b56a00758fd
Julian Gojani <julian@gojani.xyz>
2021-03-23 23:11:24 +0100

    Added runCommand method and more for WebFrameworkTest

--- src/main/java/org/javawebstack/framework/testing/WebFrameworkTest.java
+++ src/main/java/org/javawebstack/framework/testing/WebFrameworkTest.java
@@ -1,6 +1,8 @@
 package org.javawebstack.framework.testing;
 
+import org.javawebstack.command.CommandResult;
 import org.javawebstack.framework.WebApplication;
+import org.javawebstack.framework.config.Config;
 import org.javawebstack.httpserver.test.HTTPTest;
 import org.javawebstack.injector.Injector;
 
@@ -27,5 +29,17 @@ public abstract class WebFrameworkTest extends HTTPTest {
 
     public <T> T inject(Class<T> clazz, String name){
         return webApplication.getInjector().getInstance(clazz, name);
+    }
+
+    public void seed(String name){
+        webApplication.getSeeder(name).seed();
+    }
+
+    public CommandResult runCommand(String... input){
+        return webApplication.getCommandSystem().eval(input);
+    }
+
+    public Config getConfig(){
+        return webApplication.getConfig();
     }
 }
