484f00489a832ce73097fa779a3628675c71f425
JanHolger <jan@bebendorf.eu>
2021-03-24 20:56:42 +0100

    Added Context to Resource to allow resource mapping based on additional context

--- src/main/java/org/javawebstack/framework/util/Resource.java
+++ src/main/java/org/javawebstack/framework/util/Resource.java
@@ -1,32 +1,53 @@
 package org.javawebstack.framework.util;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public interface Resource<T> {
 
-    void map(T source);
+    void map(T source, Context context);
 
     static <T> Resource<T> make(Class<? extends Resource<T>> type, T source) {
+        return make(type, new Context(), source);
+    }
+
+    static <T> List<Resource<T>> make(Class<? extends Resource<T>> type, List<T> source) {
+        return make(type, new Context(), source);
+    }
+
+    static <T> List<Resource<T>> make(Class<? extends Resource<T>> type, T... source) {
+        return make(type, new Context(), source);
+    }
+
+    static <T> Resource<T> make(Class<? extends Resource<T>> type, Context context, T source) {
         if (source == null)
             return null;
         try {
             Resource<T> resource = type.newInstance();
-            resource.map(source);
+            resource.map(source, context);
             return resource;
         } catch (InstantiationException | IllegalAccessException e) {
             return null;
         }
     }
 
-    static <T> List<Resource<T>> make(Class<? extends Resource<T>> type, List<T> source) {
-        return source.stream().map(s -> make(type, s)).filter(Objects::nonNull).collect(Collectors.toList());
+    static <T> List<Resource<T>> make(Class<? extends Resource<T>> type, Context context, List<T> source) {
+        return source.stream().map(s -> make(type, context, s)).filter(Objects::nonNull).collect(Collectors.toList());
     }
 
-    static <T> List<Resource<T>> make(Class<? extends Resource<T>> type, T... source) {
-        return make(type, Arrays.asList(source));
+    static <T> List<Resource<T>> make(Class<? extends Resource<T>> type, Context context, T... source) {
+        return make(type, context, Arrays.asList(source));
+    }
+
+    class Context {
+        final Map<String, Object> attribs = new HashMap<>();
+        public <T> T attrib(String key) {
+            return (T) attribs.get(key);
+        }
+        public Context attrib(String key, Object value) {
+            attribs.put(key, value);
+            return this;
+        }
     }
 
 }
